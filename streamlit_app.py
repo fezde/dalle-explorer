defc985c40a8b9b3319b57a4974d2da4f01b5915
fezde <fezde@users.noreply.github.com>
2023-03-22 21:14:04 +0100

    Update streamlit_app.py
    smaller image + 2 cols

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,17 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 key = st.text_input("API Key")
 prompt = st.text_area("Prompt")
+
+col1, col2, col3 = st.columns([1,3])
+
 if st.button('GO'):
     openai.api_key = key
     response = openai.Image.create(
         prompt=prompt,
         n=1,
-        size="1024x1024"
+        size="256x256"
     )
-
-    st.write(prompt)
-    st.image(response['data'][0]['url'])
+    with col1:
+        st.write(prompt)
+    with col2:
+        st.image(response['data'][0]['url'])
