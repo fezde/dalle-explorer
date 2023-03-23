a7577682a226f3a1a4bd8a462772cf78ffcae11a
Felix Kratzer <felix@pace.car>
2023-03-23 09:19:23 +0100

    Make it interactive

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,30 @@
 import openai
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def go():
+    with st.spinner('Wait for it...'):
+        openai.api_key = key
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="256x256"
+        )
+        st.session_state.images.insert(0, response['data'][0]['url'])
+        st.session_state.prompts.insert(0, prompt)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if 'prompts' not in st.session_state:
+    st.session_state.prompts = []
+    st.session_state.images = []
 
-key = st.text_input("API Key")
+key = st.text_input("API Key", type="password", help="Your openAI API Key")
 prompt = st.text_area("Prompt")
+button = st.button('GO', on_click=go)
 
-col1, col2 = st.columns([1,3])
-
-if st.button('GO'):
-    openai.api_key = key
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="256x256"
-    )
+for i in range(len(st.session_state.prompts)):
+    col1, col2 = st.columns([1, 3])
     with col1:
-        st.write(prompt)
+        st.write(st.session_state.prompts[i])
     with col2:
-        st.image(response['data'][0]['url'])
+        st.image(st.session_state.images[i])
