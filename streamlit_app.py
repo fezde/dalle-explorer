1ea902b74057b4b9b19ae580ab0ef9c5322345e1
Felix Kratzer <felix@pace.car>
2023-03-30 09:50:49 +0200

    More output options

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,12 @@ def go():
         openai.api_key = key
         response = openai.Image.create(
             prompt=prompt,
-            n=1,
-            size="256x256"
+            n=count,
+            size=size
         )
-        st.session_state.images.insert(0, response['data'][0]['url'])
+        urls = [x['url'] for x in response['data']]
+
+        st.session_state.images.insert(0, urls)
         st.session_state.prompts.insert(0, prompt)
 
 
@@ -18,8 +20,22 @@ if 'prompts' not in st.session_state:
     st.session_state.prompts = []
     st.session_state.images = []
 
-key = st.text_input("API Key", type="password", help="Your openAI API Key")
+key_from_secret = ""
+if "API_KEY" in st.secrets:
+    print("Using key from secrets")
+    key_from_secret = st.secrets["API_KEY"]
+
+key = st.text_input("API Key", type="password", help="Your openAI API Key", value=key_from_secret)
 prompt = st.text_area("Prompt")
+size = st.selectbox(
+    'Size in pixels',
+    ("256x256", "512x512", "1024x1024"))
+count = st.slider(
+    "How many images should be requested",
+    value=1,
+    min_value=1,
+    max_value=10
+)
 button = st.button('GO', on_click=go)
 
 for i in range(len(st.session_state.prompts)):
@@ -27,4 +43,5 @@ for i in range(len(st.session_state.prompts)):
     with col1:
         st.write(st.session_state.prompts[i])
     with col2:
-        st.image(st.session_state.images[i])
+        for url in st.session_state.images[i]:
+            st.image(url)
